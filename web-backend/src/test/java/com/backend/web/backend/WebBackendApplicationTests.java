8d7f3e4e26dc4a5aa61e4ad2e66e50c1d8d36bfa
S-Farsana <farsanasujah@gmail.com>
2025-12-15 17:38:11 +0530

    folders created

---
+++ web-backend/src/test/java/com/backend/web/backend/WebBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.web.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

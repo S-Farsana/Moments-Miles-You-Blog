7424f940ab047914586981d30e989bc747f34c76
S-Farsana <farsanasujah@gmail.com>
2025-12-24 00:14:01 +0530

    landingPage24-11-25

---
+++ Web Frontend/src/Pages/LandingPage/LandingPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '/LandingPage.css'
+
+function LandingPage() {
+  return (
+    <div>
+        <h1>Moments Miles and You</h1>
+      
+    </div>
+  )
+}
+
+export default LandingPage
--- Web Frontend/src/main.jsx
+++ Web Frontend/src/main.jsx
@@ -1,5 +1,12 @@
 import { render } from 'preact'
 import './index.css'
 import { App } from './app.jsx'
+import { BrowserRouter } from 'react-router-dom'
 
-render(<App />, document.getElementById('app'))
+createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+  <StrictMode>
+    <App />
+  </StrictMode>
+  </BrowserRouter>,
+)
